1ce4766d59ab29a62a6d4bd51c4f20993ca8079d
Arthur Granado <agranado@pluga.co>
2018-01-17 15:00:25 -0200

    Complete HtC1x coursce - miss final project

---
+++ HtC1x/space-invaders-starter.rkt
@@ -0,0 +1,187 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname space-invaders-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/universe)
+(require 2htdp/image)
+
+;; Space Invaders
+
+
+;; Constants:
+
+(define WIDTH  300)
+(define HEIGHT 500)
+
+(define INVADER-X-SPEED 1.5)  ;speeds (not velocities) in pixels per tick
+(define INVADER-Y-SPEED 1.5)
+(define TANK-SPEED 2)
+(define MISSILE-SPEED 10)
+
+(define HIT-RANGE 10)
+
+(define INVADE-RATE 100)
+
+(define BACKGROUND (empty-scene WIDTH HEIGHT))
+
+(define INVADER
+  (overlay/xy (ellipse 10 15 "outline" "blue")              ;cockpit cover
+              -5 6
+              (ellipse 20 10 "solid"   "blue")))            ;saucer
+
+(define TANK
+  (overlay/xy (overlay (ellipse 28 8 "solid" "black")       ;tread center
+                       (ellipse 30 10 "solid" "green"))     ;tread outline
+              5 -14
+              (above (rectangle 5 10 "solid" "black")       ;gun
+                     (rectangle 20 10 "solid" "black"))))   ;main body
+
+(define TANK-HEIGHT/2 (/ (image-height TANK) 2))
+
+(define MISSILE (ellipse 5 15 "solid" "red"))
+
+
+
+;; Data Definitions:
+
+(define-struct game (invaders missiles tank))
+;; Game is (make-game  (listof Invader) (listof Missile) Tank)
+;; interp. the current state of a space invaders game
+;;         with the current invaders, missiles and tank position
+
+;; Game constants defined below Missile data definition
+
+#;
+(define (fn-for-game s)
+  (... (fn-for-loinvader (game-invaders s))
+       (fn-for-lom (game-missiles s))
+       (fn-for-tank (game-tank s))))
+
+
+
+(define-struct tank (x dir))
+;; Tank is (make-tank Number Integer[-1, 1])
+;; interp. the tank location is x, HEIGHT - TANK-HEIGHT/2 in screen coordinates
+;;         the tank moves TANK-SPEED pixels per clock tick left if dir -1, right if dir 1
+
+(define T0 (make-tank (/ WIDTH 2) 1))   ;center going right
+(define T1 (make-tank 50 1))            ;going right
+(define T2 (make-tank 50 -1))           ;going left
+
+#;
+(define (fn-for-tank t)
+  (... (tank-x t) (tank-dir t)))
+
+
+
+(define-struct invader (x y dx))
+;; Invader is (make-invader Number Number Number)
+;; interp. the invader is at (x, y) in screen coordinates
+;;         the invader along x by dx pixels per clock tick
+
+(define I1 (make-invader 150 100 12))           ;not landed, moving right
+(define I2 (make-invader 150 HEIGHT -10))       ;exactly landed, moving left
+(define I3 (make-invader 150 (+ HEIGHT 10) 10)) ;> landed, moving right
+
+
+#;
+(define (fn-for-invader invader)
+  (... (invader-x invader) (invader-y invader) (invader-dx invader)))
+
+
+(define-struct missile (x y))
+;; Missile is (make-missile Number Number)
+;; interp. the missile's location is x y in screen coordinates
+
+(define M1 (make-missile 150 300))                               ;not hit U1
+(define M2 (make-missile (invader-x I1) (+ (invader-y I1) 10)))  ;exactly hit U1
+(define M3 (make-missile (invader-x I1) (+ (invader-y I1)  5)))  ;> hit U1
+(define M4 (make-missile 300 250))                               ;not hit U1
+
+#;
+(define (fn-for-missile m)
+  (... (missile-x m) (missile-y m)))
+
+
+
+(define G0 (make-game empty empty T0))
+(define G1 (make-game empty empty T2))
+(define G2 (make-game (list I1) (list M1) T1))
+(define G3 (make-game (list I1 I2) (list M1 M2) T1))
+(define G4 (make-game (list I1 I2) (list M1 M4) T1))
+
+
+;; =================
+;; Functions:
+
+;; Game -> Game
+;; start the world with ...
+;; 
+(define (main g)
+  (big-bang g                       ; Game
+    (on-tick   next-game)   ; Game -> Game
+    (to-draw   render-game) ; Game -> Image
+    (on-key    handle-key))); Game KeyEvent -> Game
+
+;; Game -> Game
+;; produce the next Game:
+;; - update all invaders, missile and tank positions,
+;; - identify if missile hit invaders
+;; - generate new invaders on top screen randomly
+(check-expect (next-game G0)
+              (make-game empty empty (make-tank (+ (/ WIDTH 2) TANK-SPEED) 1)))
+(check-expect (next-game G1)
+              (make-game empty empty (make-tank (- (/ WIDTH 2) TANK-SPEED) -1)))
+(check-expect (next-game G2)
+              (make-game (list (make-invader (+ 150 INVADER-X-SPEED) (+ 100 INVADER-Y-SPEED) 12)) ;; for what I'll use dx?
+                         (list (make-missile 150 (- 300 MISSILE-SPEED)))
+                         (make-tank (+ 50 TANK-SPEED) 1)))
+(check-expect (next-game G3)
+              (make-game (list (make-invader (+ 150 INVADER-X-SPEED) (+ HEIGHT INVADER-Y-SPEED) -10))
+                         (list (make-missile 150 (- 300 MISSILE-SPEED)))
+                         (make-tank (+ 50 TANK-SPEED) 1)))
+
+;(define (next-game g) g) ;stub
+
+(define (next-game g)
+  (detect-colitions (update-positions (generate-invaders g))))
+
+;; Game -> Game
+;; produce new invaders randomly
+;; !!!
+(define (generate-invaders g) g) ; stub
+
+;; Game -> Game
+;; update positions for invaders, missile, and tank
+(check-expect (next-game G0)
+              (make-game empty empty (make-tank (+ (/ WIDTH 2) TANK-SPEED) 1)))
+(check-expect (next-game G1)
+              (make-game empty empty (make-tank (- (/ WIDTH 2) TANK-SPEED) -1)))
+(check-expect (next-game G2)
+              (make-game (list (make-invader (+ 150 INVADER-X-SPEED) (+ 100 INVADER-Y-SPEED) 12)) ;; for what I'll use dx?
+                         (list (make-missile 150 (- 300 MISSILE-SPEED)))
+                         (make-tank (+ 50 TANK-SPEED) 1)))
+(check-expect (next-game G4)
+              (make-game (list (make-invader (+ 150 INVADER-X-SPEED) (+ 100 INVADER-Y-SPEED) 12) I2) (list M1 M4) T1)
+              
+
+(define (update-positions g) g) ; stub
+
+;; Game -> Game
+;; dectect with any missile colided with some ships, if true, remove both
+;; !!!
+(define (detect-colitions g) g) ; stub
+
+;; Game -> Image
+;; render all imagens on screen for invaders, missiles and tank 
+;; !!!
+(define (render-game g) BACKGROUND)
+
+
+;; Game KeyEvent -> Game
+;; change the tank direction by pressing the left and right arrow keys
+;; and shot missiles pressing space key
+;; !!!
+(define (handle-key g ke)
+  (cond [(key=? ke " ") g]
+        [else 
+         g]))
